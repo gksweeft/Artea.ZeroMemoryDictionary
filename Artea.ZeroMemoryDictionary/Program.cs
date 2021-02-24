363747d96f8be362eb4c8782b8dfff82e1498ee2
GKSweeft <g.kesanashvili@sweeftdigital.com>
2021-02-24 13:19:10 +0400

    Add project files.

---
+++ Artea.ZeroMemoryDictionary/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Artea.ZeroMemoryDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict1 = new ZeroMemoryDictionary();
+
+            dict1.Add("1", "some text");
+            dict1.TryAdd("1", "some more text");
+            dict1.Add("2", "some text 2");
+            dict1["2"] = "some more text 2";
+
+            Console.WriteLine(nameof(dict1));
+            Console.WriteLine($"Count: {dict1.Count}");
+
+            foreach (var item in dict1)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=====================================");
+            Console.WriteLine();
+
+            var dict2 = new ZeroMemoryDictionary();
+
+            dict2.Add("1", "some text");
+
+            Console.WriteLine(nameof(dict2));
+            Console.WriteLine($"Count: {dict2.Count}");
+
+            foreach (var item in dict2)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=====================================");
+            Console.WriteLine();
+
+            var namedDict1 = new ZeroMemoryDictionary("namedStore");
+
+            namedDict1.TryAdd("1", "some text");
+            namedDict1.TryAdd("1", "some more text");
+            namedDict1.TryAdd("2", "some text 2");
+            namedDict1["2"] = "some more text 2";
+
+            Console.WriteLine(nameof(namedDict1));
+            Console.WriteLine($"Count: {namedDict1.Count}");
+
+            foreach (var item in namedDict1)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=====================================");
+            Console.WriteLine();
+
+            namedDict1.Remove("1");
+
+            var namedDict2 = new ZeroMemoryDictionary("namedStore");
+
+            namedDict2.TryAdd("3", "some text 23");
+            namedDict2.TryAdd("4", "some text 34234");
+
+            Console.WriteLine(nameof(namedDict2));
+            Console.WriteLine($"Count: {namedDict2.Count}");
+
+            foreach (var item in namedDict2)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+    }
+}
---
+++ Artea.ZeroMemoryDictionary/ZeroMemoryDictionary.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Artea.ZeroMemoryDictionary
+{
+    public class ZeroMemoryDictionary : IDictionary<string, string>
+    {
+        private readonly string _fileName;
+
+        public ZeroMemoryDictionary(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public ZeroMemoryDictionary() : this($"{Guid.NewGuid()}.json")
+        {
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                var data = Read();
+                return data[key];
+            }
+
+            set
+            {
+                var data = Read();
+                data[key] = value;
+                Write(data);
+            }
+        }
+
+        public ICollection<string> Keys
+        {
+            get
+            {
+                var data = Read();
+                return data.Keys;
+            }
+        }
+
+        public ICollection<string> Values
+        {
+            get
+            {
+                var data = Read();
+                return data.Values;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                var data = Read();
+                return data.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                var data = Read();
+                return data.IsReadOnly;
+            }
+        }
+
+        public void Add(string key, string value)
+        {
+            var data = Read();
+
+            data.Add(key, value);
+
+            Write(data);
+        }
+
+        public void Add(KeyValuePair<string, string> item) => Add(item.Key, item.Value);
+
+        public void Clear() => Write(new Dictionary<string, string>());
+
+        public bool Contains(KeyValuePair<string, string> item)
+        {
+            var data = Read();
+
+            return data.Contains(item);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            var data = Read();
+
+            return data.ContainsKey(key);
+        }
+
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            var data = Read();
+
+            data.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            var data = Read();
+            return data.GetEnumerator();
+        }
+
+        public bool Remove(string key)
+        {
+            var data = Read();
+
+            bool result = data.Remove(key);
+
+            if (result)
+            {
+                Write(data);
+            }
+
+            return result;
+        }
+
+        public bool Remove(KeyValuePair<string, string> item)
+        {
+            var data = Read();
+
+            bool result = data.Remove(item);
+
+            if (result)
+            {
+                Write(data);
+            }
+
+            return result;
+        }
+
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out string value)
+        {
+            var data = Read();
+
+            return data.TryGetValue(key, out value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            var data = Read();
+
+            return ((IEnumerable)data).GetEnumerator();
+        }
+
+        private IDictionary<string, string> Read()
+        {
+            if (!File.Exists(_fileName))
+            {
+                return new Dictionary<string, string>();
+            }
+
+            string jsonString = File.ReadAllText(_fileName);
+            return JsonSerializer.Deserialize<IDictionary<string, string>>(jsonString);
+        }
+
+        private void Write(IDictionary<string, string> data)
+        {
+            string jsonString = JsonSerializer.Serialize(data);
+            File.WriteAllText(_fileName, jsonString);
+        }
+    }
+}
